7394e2e09f41fc0173a5b0fbe920b09def59e44b
Ashish187 <ashishanand16086@gmail.com>
2021-11-22 14:25:42 +0530

    first-commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Home.css'
+import img1 from '../images/b.jpg'
+import img2 from '../images/l.jpg'
+import img3 from '../images/phone.jpg'
+
+const Home = () => {
+    return (
+        <>
+        <div>
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={img1} className="d-block w-100" alt="image1" />
+    </div>
+    <div className="carousel-item">
+      <img src={img2} className="d-block w-100" alt="image2" />
+    </div>
+    <div className="carousel-item">
+      <img src={img3} className="d-block w-100" alt="image3" />
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+<div className='sale'>
+   <h1>SALE IS LIVE NOW</h1>
+   <h3>10% instant discount* with Sbi credit card & also on Emi Trxns</h3>
+</div>
+</div>
+        </>
+        
+    )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container">
+    <Link className="navbar-brand"  to="/">ShoppingCart</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Link</Link>
+        </li>
+        <li className="nav-item dropdown">
+          <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </Link>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><Link className="dropdown-item" to="/">Action</Link></li>
+            <li><Link className="dropdown-item" to="/">Another action</Link></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><Link className="dropdown-item" to="/">Something else here</Link></li>
+          </ul>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      <ul className='cart'>
+        <li className='cart_link'>
+          <Link className='allLink' to="/cart">
+          <i className="fas fa-shopping-cart"></i>
+          <span className='cart_logo'>
+          cart
+            <span className='logo'>0</span>
+          </span>
+            
+          </Link>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+        </div>
+    )
+}
+
+export default Navbar
