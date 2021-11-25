91790c8464107d21b5fd724ba92865bccd74c5dd
Ashish187 <ashishanand16086@gmail.com>
2021-11-25 18:43:58 +0530

    fifth commit

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,13 +5,13 @@ const Checkout = () => {
     const state = useSelector((state)=>state.handleCart)
     var total = 0
     const list = (item) =>{
-        total+=item.price
+        total+=item.qty*item.price
         return (
             <li className="list-group-item d-flex justify-content-between lh-sm">
             <div>
               <h6 className="my-0">{item.title}</h6>
             </div>
-            <span className="text-muted">Rs.{item.price}</span>
+            <span className="text-muted">Rs.{item.qty*item.price}</span>
           </li>
         )
     }
